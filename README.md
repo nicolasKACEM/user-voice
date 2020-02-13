7b097f88a5603cfd93979011f78985054f83195f
nicolasKACEM <kacem.nicolas@gmail.com>
2020-02-13 10:10:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nq9f5z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nq9f5z)
---
+++ src/app/ajouter-idee/ajouter-idee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajouter-idee',
+  templateUrl: './ajouter-idee.component.html',
+  styleUrls: ['./ajouter-idee.component.css']
+})
+export class AjouterIdeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
