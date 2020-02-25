0175986177d25e6684344f9c4f57ab1e72969179
nicolasKACEM <kacem.nicolas@gmail.com>
2020-02-25 09:38:38 +0100

    2 files modified

--- src/app/liste-message/liste-message.component.ts
+++ src/app/liste-message/liste-message.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MessagesService} from  '../Service/messages.service';
-
+import {userService} from  '../Service/users.service';
 
 @Component({
   selector: 'app-liste-message',
@@ -9,13 +9,16 @@ import {MessagesService} from  '../Service/messages.service';
 })
 export class ListeMessageComponent implements OnInit {
 
-  constructor(private messageService: MessagesService) { }
+  public messages:String[];
+
+  constructor(private messageService: MessagesService, private userService) { }
 
   ngOnInit() {
+    this.messages=this.messageService.getMessages();
 
 if(!this.messageService.getMessages()) return;
     for(let i=0;i<this.messageService.getMessages().length;i++){
-      console.log(this.messageService.getMessages()[i])
+      console.log("dans la liste"+this.messageService.getMessages()[i])
     }
   }
 
