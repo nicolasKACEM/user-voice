93df4ed56c387d630ce9d54487c9050e1b9fb903
nicolasKACEM <kacem.nicolas@gmail.com>
2020-02-28 12:38:48 +0100

    1 file modified

--- src/app/Service/is-signed-in-guard.guard.ts
+++ src/app/Service/is-signed-in-guard.guard.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import {UsersService} from  '../Service/users.service';
 
-@Injectable()
+@Injectable({
+  providedIn:'root'
+})
 export class IsSignedInGuardGuard implements CanActivate {
+
+    constructor( private userService: UsersService){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    if(this.userService.user.pseudo!=null)
+      return (this.userService.user.pseudo.trim()!="");
+    return false
   }
 }
